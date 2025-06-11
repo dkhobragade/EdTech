9afcaf3115d74184b8714401c7157115c0c57060
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-12 00:18:19 +0530

    feat: enhance course and PYQ dropdowns with additional options and icons

--- frontend/app/mpsc/page.tsx
+++ frontend/app/mpsc/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import men from '../../public/man.svg'
 import arrow from '../../public/arrow.svg'
 import { TextFlipContainer } from '@/components/ui/TextFlipContainer';
-import { IconArrowBadgeDownFilled } from '@tabler/icons-react';
+import { IconArchiveFilled, IconArrowBadgeDownFilled, IconClockQuestion, IconDownload, IconWriting } from '@tabler/icons-react';
 import { Timeline } from '@/components/ui/timeline';
 import { timelineData } from '@/helper/Constants';
 
@@ -12,15 +12,65 @@ const page = () =>
     return (
         <div className="p-5 gap-11">
             <div className="flex flex-row items-end justify-end p-4 gap-10 ">
-                <div className="cursor-pointer flex gap-1">
-                    Courses
-                    <span>
-                        <IconArrowBadgeDownFilled />
-                    </span>
+                <div className='relative group'>
+                    <div className="cursor-pointer flex gap-1 items-center">
+                        Courses
+                        <span>
+                            <IconArrowBadgeDownFilled />
+                        </span>
+                    </div>
+                    <div className="absolute left-0 mt-2 z-10 hidden group-hover:block group-focus-within:block ">
+                        <div
+                            tabIndex={ 0 }
+                            className='w-25 rounded-b-2xl h-25 bg-[#FFE68C] flex flex-col justify-center items-center shadow-lg outline-none'
+                        >
+                            <div className='grid grid-rows-2 gap-2 p-2 w-full h-full'>
+                                <div className='w-full h-full flex gap-2 cursor-pointer border-b-2 border-neutral-400 pb-2'>
+                                    Pre
+                                    <span>
+                                        <IconArchiveFilled />
+                                    </span>
+                                </div>
+                                <div className='w-full h-full flex gap-2 cursor-pointer'>
+                                    Mains
+                                    <span>
+                                        <IconWriting />
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="cursor-pointer flex gap-1">
-                    PYQ
-                    <IconArrowBadgeDownFilled />
+                    <div className='relative group'>
+                        <div className="cursor-pointer flex gap-1 items-center">
+                            PYQ
+                            <span>
+                                <IconArrowBadgeDownFilled />
+                            </span>
+                        </div>
+                        <div className="absolute left-0 mt-2 z-10 hidden group-hover:block group-focus-within:block ">
+                            <div
+                                tabIndex={ 0 }
+                                className='w-30 rounded-b-2xl h-25 bg-[#FFC9F0] flex flex-col justify-center items-center shadow-lg outline-none'
+                            >
+                                <div className='grid grid-rows-2 gap-2 p-2 w-full h-full'>
+                                    <div className='w-full h-full flex gap-2 cursor-pointer border-b-2 border-neutral-400 pb-2'>
+                                        Mock Test
+                                        <span>
+                                            <IconClockQuestion />
+                                        </span>
+                                    </div>
+                                    <div className='w-full h-full flex gap-2 cursor-pointer'>
+                                        Download
+                                        <span>
+                                            <IconDownload />
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="cursor-pointer">
                     Free Initiative
@@ -57,7 +107,7 @@ const page = () =>
                     </div>
                 </div>
             </div>
-            <Timeline data={ timelineData } />
+            {/* <Timeline data={ timelineData } /> */ }
         </div>
     )
 }
